ec1baf2a369895273e02c7c1082dd7509ce9094c
Median Hardiv Nugraha <medianhardiv@gmail.com>
2024-02-02 10:41:32 +0700

    Update app.py for model tuning and prediction

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import streamlit as st
 import pickle
 
@@ -14,43 +15,63 @@ bmi_input = st.number_input('Masukkan BMI ')
 children_input = st.number_input('Masukkan jumlah anak ')
 sex_input = st.radio('Jenis kelamin ', ['Laki-laki','Perempuan'])
 smoker_input = st.radio('Merokok? ', ['Ya','Tidak'])
-region_input = st.radio('Wilayah Tempat Tinggal', ['Northeast','Northwest','Southeast','Southwest'])
+region_input = st.radio('Wilayah Tempat Tinggal', ['Northeast','Northwest','Southeast','Southwest']).lower()
 
-sex_input_encoded = []
-smoker_input_encoded = []
-region_input_encoded = []
+# Kode jika model gapake ColumnTransformer
+
+# sex_input_encoded = []
+# smoker_input_encoded = []
+# region_input_encoded = []
+
+# if(sex_input == 'Laki-laki'):
+#     sex_input_encoded = [0,1]
+# else:
+#     sex_input_encoded = [1,0]
+
+# if(smoker_input == 'Ya'):
+#     smoker_input_encoded = [0,1]
+# else:
+#     smoker_input_encoded = [1,0]
+
+# if(region_input == 'northeast'):
+#     region_input_encoded = [1,0,0,0]
+    
+# elif(region_input == 'northwest'):
+#     region_input_encoded = [0,1,0,0]
+# elif(region_input == 'southeast'):
+#     region_input_encoded = [0,0,1,0]
+# else:
+#     region_input_encoded = [0,0,0,1]
+
+# region_input_encoded = np.array(region_input_encoded)
+# sex_input_encoded = np.array(sex_input_encoded)
+# smoker_input_encoded = np.array(smoker_input_encoded)
+
+# data_encoded = np.concatenate([sex_input_encoded, smoker_input_encoded, region_input_encoded], axis=None)
+
+# Kode jika model pake ColumnTransformer
 
 if(sex_input == 'Laki-laki'):
-    sex_input_encoded = [0,1]
+    sex_input = 'male'
 else:
-    sex_input_encoded = [1,0]
+    sex_input = 'female'
 
 if(smoker_input == 'Ya'):
-    smoker_input_encoded = [0,1]
+    smoker_input = 'yes'
 else:
-    smoker_input_encoded = [1,0]
-
-if(region_input == 'Northeast'):
-    region_input_encoded = [1,0,0,0]
-    
-elif(region_input == 'Northwest'):
-    region_input_encoded = [0,1,0,0]
-elif(region_input == 'Southeast'):
-    region_input_encoded = [0,0,1,0]
-else:
-    region_input_encoded = [0,0,0,1]
-
-region_input_encoded = np.array(region_input_encoded)
-sex_input_encoded = np.array(sex_input_encoded)
-smoker_input_encoded = np.array(smoker_input_encoded)
-
-data_encoded = np.concatenate([sex_input_encoded, smoker_input_encoded, region_input_encoded], axis=None)
+    smoker_input = 'no'
 
 if st.button('Prediksi Harga'):
-    data_num = np.array([age_input, bmi_input, children_input])
-    data = np.concatenate((data_num, data_encoded), axis=None).reshape(1,-1)
-    # st.write(data)
+    #Kode jika gapake ColumnTransformer
+    # data_num = np.array([age_input, bmi_input, children_input])
+    # data = np.concatenate((data_num, data_encoded), axis=None).reshape(1,-1)
+
+    #Kode jika pake ColumnTransformer
+    data_dict = {'age' : age_input, 'bmi' : bmi_input, 'children' : children_input, 'sex' : sex_input, 'smoker' : smoker_input, 'region' : region_input}
+    data = pd.DataFrame(data_dict, index=[0])
+
+    # st.write(data) # buat debugging
     result = st.session_state['model'].predict(data)
-    st.write(f'Perkiraan biaya rumah sakit Anda adalah: ${result[0]}')
+    st.write(f'Perkiraan biaya rumah sakit Anda adalah: {result[0]}')
 else:
     st.write('Masukkan data diri Anda dan model akan memprediksi biaya rumah sakit Anda!')
